3e256fe6f08aae54ba190e958414fd72d499fc13
chasuyeon <sy9600@naver.com>
2019-08-18 19:54:56 +0900

    pod setting

---
+++ HowlTalk/HowlTalk/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  HowlTalk
+//
+//  Created by 차수연 on 17/08/2019.
+//  Copyright © 2019 차수연. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+  
+  var box: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = #imageLiteral(resourceName: "icons8-talk-64")
+    return imageView
+  }()
+  
+  
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    self.view.addSubview(box)
+    box.snp.makeConstraints { (make) in
+      make.center.equalTo(self.view)
+    }
+    
+    
+  }
+
+
+}
+
